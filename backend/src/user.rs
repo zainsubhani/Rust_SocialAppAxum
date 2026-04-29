b970acd6e42ee6b4bfb26a0e3d630da0a1bcdeaf
Zain Subhani <zainsubhani@Zains-MacBook-Pro.local>
2026-04-29 16:25:14 +0200

    user api

--- backend/src/user.rs
+++ backend/src/user.rs
@@ -13,23 +13,35 @@ pub struct User {
     pub banner_url: Option<String>,
     pub created_at: OffsetDateTime,
 }
-#[derive(Debug,Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct CreateUser {
-    pub username: String ,
-    pub display_name : String
+    pub username: String,
+    pub display_name: String,
 }
 
-pub async fn get_all (pg:&PgPool) -> Result<Vec<User>> , sqlx::Error> {
-    sqlx::query_as!(User,"SELECT * from user ORDER BY username ").fetchall(pg).await
-}
-pub async fn get_by_username (pg:&PgPool ,username : &str ) -> Result<Vec<User>> , sqlx::Error> {
-    sqlx::query_as!(User,"SELECT * from user ORDER BY username=$1 ",username).fetch_optional(pg).await
+pub async fn get_all(pg: &PgPool) -> Result<Vec<User>, sqlx::Error> {
+    sqlx::query_as!(User, "SELECT * FROM users ORDER BY username")
+        .fetch_all(pg)
+        .await
 }
 
-pub async fn create(pg:&PgPool,input:CreateUser)->Result<User,sqlx::Error>{
+pub async fn get_by_username(pg: &PgPool, username: &str) -> Result<Option<User>, sqlx::Error> {
+    sqlx::query_as!(User, "SELECT * FROM users WHERE username = $1", username)
+        .fetch_optional(pg)
+        .await
+}
+
+pub async fn create(pg: &PgPool, input: CreateUser) -> Result<User, sqlx::Error> {
     sqlx::query_as!(
-        User,"
-        INSERT INTO users {username , display_name } VALUES ($1,$2) RETURNING *
-        ", input.username, input.display_name
-    ).fetch_one(pg).await
+        User,
+        "
+        INSERT INTO users (username, display_name)
+        VALUES ($1, $2)
+        RETURNING *
+        ",
+        input.username,
+        input.display_name
+    )
+    .fetch_one(pg)
+    .await
 }
